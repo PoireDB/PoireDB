ae3a5721fd8aa3d3c3c4ca8359446f88259053d9
Adi Salimgereev <68709264+AdiSalimgereev@users.noreply.github.com>
2022-01-16 21:14:35 +0600

    Change testing framework from acutest to tau (to avoid problem on Windows Github Action)

--- pql/lexer/test_lexer.c
+++ pql/lexer/test_lexer.c
@@ -1,35 +1,36 @@
-#include "../include/acutest.h"
 #include "../include/lexer/lexer.h"
+#include "../include/tau.h"
 
-void test_EOF(void) {
+TAU_MAIN() // sets up Tau (+ main function)
+
+TEST(foo, test_EOF) {
   lexer_T *l = init_lexer("<testdata>", "");
   token_T *t = next_token(l);
-  TEST_CHECK(t->type == _EOF);
+  CHECK(t->type == _EOF);
   free(t);
   free(l);
 }
 
-void test_identifier(void) {
+TEST(foo, test_identifier) {
   lexer_T *l = init_lexer(
       "<testdata>", "_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM");
   token_T *t = next_token(l);
-  TEST_CHECK(t->type == IDENTIFIER);
-  TEST_CHECK(strcmp(t->value->string,
-                    "_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM") ==
-             0);
+  CHECK(t->type == IDENTIFIER);
+  CHECK(strcmp(t->value->string,
+               "_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM") == 0);
   free(t);
   free(l);
 }
 
-void test_punctuator(void) {
+TEST(foo, test_punctuators) {
   char *punctuators = ";><=.,{}()";
   lexer_T *l = init_lexer("<testdata>", punctuators);
   token_T *t = next_token(l);
   size_t current_punctuator_index = 0;
 
   while (t->type != _EOF) {
-    TEST_CHECK(t->type == PUNCTUATOR);
-    TEST_CHECK(t->value->character == punctuators[current_punctuator_index]);
+    CHECK(t->type == PUNCTUATOR);
+    CHECK(t->value->character == punctuators[current_punctuator_index]);
     current_punctuator_index++;
     t = next_token(l);
   }
@@ -38,17 +39,17 @@ void test_punctuator(void) {
   free(l);
 }
 
-void test_boolean(void) {
+TEST(foo, test_boolean) {
   lexer_T *l = init_lexer("<testdata>", "true false");
 
   token_T *MustBeTrue = next_token(l);
   token_T *MustBeFalse = next_token(l);
 
-  TEST_CHECK(MustBeTrue->type == VBOOLEAN);
-  TEST_CHECK(MustBeFalse->type == VBOOLEAN);
+  CHECK(MustBeTrue->type == VBOOLEAN);
+  CHECK(MustBeFalse->type == VBOOLEAN);
 
-  TEST_CHECK(MustBeTrue->value->boolean);
-  TEST_CHECK(!MustBeFalse->value->boolean);
+  CHECK(MustBeTrue->value->boolean);
+  CHECK(!MustBeFalse->value->boolean);
 
   free(MustBeTrue);
   free(MustBeFalse);
@@ -56,7 +57,7 @@ void test_boolean(void) {
   free(l);
 }
 
-void test_number(void) {
+TEST(foo, test_number) {
   char *input[] = {
       "1234567890",
       "1234567890.0123456789",
@@ -68,16 +69,9 @@ void test_number(void) {
   for (size_t i = 0; i < sizeof(input) / sizeof(input[0]); i++) {
     lexer_T *l = init_lexer("<testdata>", input[i]);
     token_T *t = next_token(l);
-    TEST_CHECK(t->type == VNUMBER);
-    TEST_CHECK(t->value->number == excpt_output[i]);
+    CHECK(t->type == VNUMBER);
+    CHECK(t->value->number == excpt_output[i]);
     free(t);
     free(l);
   }
 }
-
-TEST_LIST = {{"eof", test_EOF},
-             {"identifier", test_identifier},
-             {"punctuator", test_punctuator},
-             {"boolean", test_boolean},
-             {"number", test_number},
-             {NULL, NULL}};
